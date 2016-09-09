550534e7acee44fa1421d5d91a890e5a2dec3e9e
Will Cruzen <will.cruzen@gmail.com>
2016-09-08 21:05:37 -0500

    added base url

--- server/app.js
+++ server/app.js
@@ -5,3 +5,9 @@ var path = require('path');
 app.listen( process.env.PORT || 2001, 'localhost', function(){
   console.log( "I'm listening on 2001, Dave..." );
 }); // end server up
+
+app.get('/', function(req, res){
+  console.log('base URL hit');
+  //send index file from resolved path
+  res.sendFile(path.resolve('public/index.html'));
+});
